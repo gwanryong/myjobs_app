45a96f582ae127392b67b276658c9e25674a42ed
gwanryong <shiningdoragon@icloud.com>
2019-08-28 02:24:19 +0000

    残業入力画面のエラー出力時に残業データを表示したままになるよう修正

--- app/controllers/overtimeinfos_controller.rb
+++ app/controllers/overtimeinfos_controller.rb
@@ -8,7 +8,7 @@ class OvertimeinfosController < ApplicationController
       flash[:success] = "登録されました。"
       redirect_to root_url
     else
-      @feed_items = []
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
     end
   end
